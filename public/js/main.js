3667a0a222bdea4f6fbf58617a4d1a4515b92bda
Yassine Ebd <elbodmoussiyassine@gmail.com>
2023-01-10 10:09:21 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,99 @@
+// let firstVariale = "Yassine";
+// console.log(firstVariale);
+
+// let vide = "";
+// console.log(vide);
+
+// vide = "yo";
+// console.log(vide);
+
+// let varN = null;
+
+// let varVide;
+
+// console.log(varVide + varN);
+
+// // ------------------------------------------------------------------------------------------------
+
+let string1 = "yasco"
+console.log(typeof string1);
+let nb = 23;
+console.log(typeof nb);
+let bool = true;
+console.log(typeof bool);
+
+// //------------------------------------------------------------------------------------------------
+
+// let pays = "Belgique";
+// let capitale = "Bruxelles";
+
+// console.log(capitale + " Est la capitale de la " + pays);
+
+// //------------------------------------------------------------------------------------------------
+
+// let maPhraseNew = `${capitale} est la capitale de la ${pays}`;
+
+// console.log(maPhraseNew);
+
+// //------------------------------------------------------------------------------------------------
+
+
+// let phraseLongue = `Ceci est une phrase longue qui vient de ${capitale} en ${pays} et j'ai ${nb} ans, mon surnom est ${string}`
+
+// console.log(phraseLongue);
+
+//------------------------------------------------------------------------------------------------
+
+let variable1 = 7;
+let variable2 = "7";
+
+variable1 = variable2
+console.log(variable1);
+variable1 == variable2
+console.log(variable1);
+variable1 === variable2
+console.log(variable1);
+
+
+//------------------------------------------------------------------------------------------------
+
+
+let int = 5;
+let string = "5";
+
+console.log(int+string);
+
+string = 5;
+console.log(int+string);
+
+//--------------------------------------------------------------------------------------------------
+
+var prenom = "Yassine";
+let age = 23;
+const nom = "nomnom";
+
+console.log(prenom + age + nom);
+
+
+var agee = 23;
+
+let preenom = "Yassine";
+
+console.log(preenom + agee)
+
+const ageee= 23;
+
+const preeeenom = "yassine";
+
+console.log(preeeenom + ageee);
+
+if (true) {
+    let test1 ="test1";
+    var test2 ="test2";
+    console.log(test1 + test2);
+}
+
+let test1 = "test1";
+var test2 = "test2";
+console.log(test1 + test2);
+
